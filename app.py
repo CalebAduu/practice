4fbd88f064fa3e929e3a72c98e81c1f7489dedbe
CalebAduu <90443559+CalebAduu@users.noreply.github.com>
2025-06-27 15:41:18 +0100

    Update app.py

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ def create_vector_store(text_chunks, embeddings):
 
 # Function to create LLMS model
 def create_llms_model():
-    llm = CTransformers(model="mistral-7b-instruct-v0.1.Q4_K_M.gguf", config={'max_new_tokens': 512, 'temperature': 0.01})
+    llm = CTransformers(model="TheBloke/Mistral-7B-Instruct-v0.1-GGUF", config={'max_new_tokens': 512, 'temperature': 0.01})
     return llm
 
 # Initialize Streamlit app
